0a6bbb7bc090f5af91b8691c466993f16b2ed771
GiAN0001 <132319947+GiAN0001@users.noreply.github.com>
2025-11-14 14:46:02 +0800

    Dropdown component created

--- resources/views/components/sidebar/dropdown.blade.php
+++ resources/views/components/sidebar/dropdown.blade.php
@@ -3,10 +3,16 @@
 <div x-data="{ open: {{ $active ? 'true' : 'false' }} }" class="relative">
     <button 
         @click="open = !open" 
-        class="flex items-center justify-between w-full p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 transition duration-75 group"
+        class="
+            flex items-center justify-between w-full p-2 text-sm font-medium rounded-lg transition duration-150 group
+            {{ $active ? 'bg-blue-700 text-slate-50' : 'text-slate-700 hover:bg-blue-700 hover:text-slate-50' }}
+        "
     >
         <span class="flex items-center text-left">
-            <span class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+            <span class="
+                w-6 h-6 transition duration-150 
+                {{ $active ? 'text-slate-50' : 'text-slate-700 group-hover:text-slate-50' }}
+            ">
                 {{ $icon ?? '' }}
             </span>
             
@@ -14,14 +20,17 @@
         </span>
 
         <svg 
-            class="w-3 h-3 transition-transform duration-200" 
+            class="
+                w-3 h-3 transition-transform duration-200 
+                {{ $active ? 'text-slate-50' : 'text-slate-700 group-hover:text-slate-50' }}
+            " 
             :class="{ 'rotate-180': open }" 
             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
         </svg>
     </button>
 
-    <ul x-show="open" x-transition class="py-2 space-y-2 pl-10">
+    <ul x-show="open" x-transition class="py-2 space-y-1 pl-6">
         {{ $slot }}
     </ul>
 </div>
