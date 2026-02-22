32aa2fdcf702a771c4497e51dc8f94e99af19fb8
Atakieni <kaithly.nico@gmail.com>
2026-02-22 21:40:45 +0800

    not yet finished, made some minimal changes

--- app/Models/FacilityReservation.php
+++ app/Models/FacilityReservation.php
@@ -10,31 +10,44 @@ class FacilityReservation extends Model
     use HasFactory;
 
     protected $table = 'facility_reservations';
-    
+    //Modified by Cath
     protected $fillable = [
         'facility_id',
         'event_name',
-        'processed_by_user_id',
-        'resident_id',
+        'purpose_category',
         'resident_type',
+        'resident_id',
         'renter_name',
         'renter_contact',
-        'time_start',
-        'time_end',
+        'email',
         'start_date',
         'end_date',
+        'time_start',
+        'time_end',
         'status',
-        'mode_of_payment',
-        'payment_status',
-        'fee',
+        'processed_by_user_id',
+        'transferred_for_payment_by_user_id',
+        'transferred_paid_by_user_id',
+        'cancelled_by_user_id',
+        'rejected_by_user_id',
+        'for_payment_at',
+        'paid_at',
+        'date_of_cancelled',
+        'date_of_rejected',
     ];
 
     protected $casts = [
         'start_date' => 'date',
         'end_date' => 'date',
-        'fee' => 'decimal:2',
+        'time_start' => 'datetime',
+        'time_end' => 'datetime',
+        'for_payment_at' => 'datetime',
+        'paid_at' => 'datetime',
+        'date_of_cancelled' => 'datetime',
+        'date_of_rejected' => 'datetime',
     ];
 
+    // Relationships
     public function equipments()
     {
         return $this->belongsToMany(Equipment::class, 'reservation_equipment', 'facility_reservation_id', 'equipment_id')
@@ -56,4 +69,25 @@ class FacilityReservation extends Model
     {
         return $this->belongsTo(User::class, 'processed_by_user_id');
     }
-}
+
+    public function transferredForPaymentBy()
+    {
+        return $this->belongsTo(User::class, 'transferred_for_payment_by_user_id');
+    }
+
+    public function transferredPaidBy()
+    {
+        return $this->belongsTo(User::class, 'transferred_paid_by_user_id');
+    }
+
+    public function cancelledBy()
+    {
+        return $this->belongsTo(User::class, 'cancelled_by_user_id');
+    }
+
+    public function rejectedBy()
+    {
+        return $this->belongsTo(User::class, 'rejected_by_user_id');
+    }
+} 
+//-----------------------
